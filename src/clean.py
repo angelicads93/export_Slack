5ad402763cc408a4ae7f8136d24c69811a1f5a54
angelicads93 <amg16k@fsu.edu>
2025-04-02 18:02:19 -0400

    Added docstring to clean.py

--- src/clean.py
+++ src/clean.py
@@ -1,28 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+@author: Angelica Goncalves.
 
+Module to clean the format of custom Pandas dataframes.
+
+Functions
+---------
+replace_empty_space(df, column, missing_value)
+    Replace empty-cell with a chosen string for a given column of the df.
+
+replace_NA(df, column, missing_value)
+    Replace NA with a chosen string for a given column of the df.
+
+handle_missing_values(df, missing_value)
+    Returns df after replacing NA in all the columns of the a dataframe."
+
+drop_rows(df, indices)
+    Drop the rows of a dataframe with the specified indices.
+
+reset_indices(df)
+    Reset the indices of a dataframe.
+
+"""
+
+# Import standard libraries:
 import numpy as np
 import pandas as pd
 
 
 def replace_empty_space(df, column, missing_value):
-    """ Replaces empty spaces "" with the string missing_value for
-    a given dataframe column
+    """
+    Replace empty-cell with a chosen string for a given column of the df.
+
+    Arguments
+    ---------
+    df : Pandas dataframe
+
+    columns : str
+        Name of the column.
+
+    missing_value : str
+        Name to represent missing values in a dataframe.
+
+    Returns
+    -------
+    df : Pandas dataframe
+
     """
     for i in range(len(df)):
         if df.at[i, column] == "":
             df.at[i, column] = missing_value
 
 
-def replace_NaN(df, column, missing_value):
-    """ Replaces missing values with the string missing_value for
-    a given dataframe column
+def replace_NA(df, column, missing_value):
+    """
+    Replace NA with a chosen string for a given column of the df.
+
+    Arguments
+    ---------
+    df : Pandas dataframe
+
+    columns : str
+        Name of the column.
+
+    missing_value : str
+        Name to represent missing values in a dataframe.
+
+    Returns
+    -------
+    df : Pandas dataframe
+
     """
     df[column] = df[column].fillna(missing_value)
 
 
 def handle_missing_values(df, missing_value):
-    """ Replaces missing values in all the columns of the a dataframe"""
+    """
+    Return df after replacing NA in all the columns of the a dataframe.
+
+    Arguments
+    ---------
+    df : Pandas dataframe
+
+    missing_value : str
+        Name to represent missing values in a dataframe
+
+    Returns
+    -------
+    df : Pandas dataframe
+
+    """
     df = df.replace(pd.NaT, missing_value)
     df = df.replace(np.nan, missing_value)
     df = df.fillna(missing_value)
@@ -30,16 +98,39 @@ def handle_missing_values(df, missing_value):
 
 
 def drop_rows(df, indices):
-    """ Drops the rows of a dataframe with the specified indices """
+    """
+    Drop the rows of a dataframe with the specified indices.
+
+    Arguments
+    ---------
+    df : Pandas dataframe
+
+    indices : list
+        List of indices to be dropped from the dataframe
+
+    Returns
+    -------
+    df : Pandas dataframe
+
+    """
     try:
         return df.drop(indices, axis=0)
-    except:
+    except Exception:
         return df
 
 
 def reset_indices(df):
-    """ Resets the indices of a dataframe.
-    Often necessary after droping rows
+    """
+    Reset the indices of a dataframe.
+
+    Arguments
+    ---------
+    df : Pandas dataframe
+
+    Returns
+    -------
+    df : Pandas dataframe
+
     """
     df.index = np.arange(0, len(df), 1)
     return df
